4b72ddce4b0ee988b15830fb73115f704e491a42
ColinVanderMeer <colinvandermeer@gmail.com>
2025-07-26 14:48:22 -0400

    Add example interactions

--- player.gd
+++ player.gd
@@ -7,6 +7,8 @@ var sprite_direction = "S": get = _get_sprite_direction
 
 @onready var sprite = $AnimatedSprite2D
 
+@onready var all_interactions = []
+@onready var interactLabel = $"InteractionComponents/Label"
 
 func _physics_process(_delta):
 	velocity = input_direction * SPEED
@@ -15,6 +17,9 @@ func _physics_process(_delta):
 	set_animation("Walk")
 	if velocity == Vector2.ZERO:
 		set_animation("Idle")
+	
+	if Input.is_action_just_pressed("interact"):
+		execute_interaction()
 
 
 func set_animation(animation):
@@ -40,3 +45,24 @@ func _get_sprite_direction():
 		Vector2.DOWN:
 			sprite_direction = "S"
 	return sprite_direction
+
+
+func _on_interaction_area_area_entered(area):
+	all_interactions.insert(0, area)
+	update_interactions()
+
+func _on_interaction_area_area_exited(area):
+	all_interactions.erase(area)
+	update_interactions()
+	
+func update_interactions():
+	if all_interactions:
+		interactLabel.text = all_interactions[0].interact_label
+	else:
+		interactLabel.text = ""
+
+func execute_interaction():
+	if all_interactions:
+		var current_interaction = all_interactions[0]
+		match current_interaction.interact_type:
+			"print_text": print(current_interaction.interact_value)
