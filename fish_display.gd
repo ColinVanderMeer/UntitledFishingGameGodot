6955ce0d3e616b48f04f04411235480756089c64
ColinVanderMeer <colinvandermeer@gmail.com>
2025-07-31 17:52:04 -0400

    Make fish list

---
+++ fish_display.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+@export var interact_type = "fish_display"
+@export var frame_num: int
+@export var current_fish: Global.fish_list
+@export var weight: float
--- global.gd
+++ global.gd
@@ -2,3 +2,5 @@ extends Node
 
 var interact = false # I should add a punch card...
 var player_spawn_position = Vector2.ZERO
+
+enum fish_list {clown, cod, salmon, pufferfish, tuna, sea_bass, catfish, pig_fish, singing_fish, machine_fish, drimp, shark, orpheus, at_fish, magical_frog}
