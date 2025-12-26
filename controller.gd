1340a3deb0797ae639072a82607771d70b203b19
ColinVanderMeer <colinvandermeer@gmail.com>
2025-12-25 22:55:14 -0500

    Cleanup

--- controller.gd
+++ controller.gd
@@ -1,19 +1,16 @@
 extends XRController3D
 
 func _on_button_pressed(action_name: String):
-	print(action_name)
 	if action_name == "ax_button":
 		var ev = InputEventAction.new()
 		ev.action = "interact"
 		ev.pressed = true
 		Input.parse_input_event(ev)
-		print("axpress")
 	if action_name == "by_button":
 		var ev = InputEventAction.new()
 		ev.action = "MenuButton"
 		ev.pressed = true
 		Input.parse_input_event(ev)
-		print("bypress")
 
 func _on_button_released(action_name: String):
 	if action_name == "ax_button":
