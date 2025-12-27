e85a5c81039777d144a24b962e8a6b1fdbf39996
ColinVanderMeer <colinvandermeer@gmail.com>
2025-12-27 14:36:52 -0500

    Update EasterEgg

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,94 +1,66 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
-const MOUSE_LOOK_SENS = 0.01
-const CONTROLLER_LOOK_SENS = 2.5
+const SNAP_THRESHOLD = 0.5 
 
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
-@onready var raycaster := $Neck/Camera3D/RayCast3D
+@onready var sprite := $Neck/AnimatedSprite3D
 
-@onready var fishAlert = get_tree().get_nodes_in_group("fishAlert")[0]
-@onready var fishSpin = get_tree().get_nodes_in_group("fishSpin")[0]
-@onready var fishMusic = fishSpin.get_node("Funky")
+@onready var xr_nodes = get_tree().get_nodes_in_group("XrNodes")[0]
+@onready var left_controller: XRController3D = xr_nodes.get_node("LeftHand")
+@onready var right_controller: XRController3D = xr_nodes.get_node("RightHand")
 
+var can_snap_turn = true
+var sprite_direction = "W"
 
-var fishing = false
-var fish_hooked = false
-
-var rng = RandomNumberGenerator.new()
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion && !fishing:
-			neck.rotate_y(-event.relative.x * MOUSE_LOOK_SENS)
-			camera.rotate_x(-event.relative.y * MOUSE_LOOK_SENS)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+func _ready():
+	Global.save_data()
 
 func _physics_process(delta: float) -> void:
-	# Controller right stick look
-	var look_x = Input.get_action_strength("look_right") - Input.get_action_strength("look_left")
-	var look_y = Input.get_action_strength("look_down") - Input.get_action_strength("look_up")
+	var rotation_input = right_controller.get_vector2("primary")
+	
+	if abs(rotation_input.x) > SNAP_THRESHOLD:
+		if can_snap_turn:
+			var turn_dir = -1 if rotation_input.x > 0 else 1
+			neck.rotate_y(turn_dir * (PI / 4.0))
+			can_snap_turn = false
+			update_sprite_direction()
+	else:
+		can_snap_turn = true
 
-	if abs(look_x) > 0.01 or abs(look_y) > 0.01 && !fishing:
-		neck.rotate_y(-look_x * CONTROLLER_LOOK_SENS * delta)
-		camera.rotate_x(-look_y * CONTROLLER_LOOK_SENS * delta)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var joy_vector = left_controller.get_vector2("primary")
+	var input_dir := Vector2.ZERO
+	
+	if joy_vector.length() > 0.1:
+		input_dir = joy_vector
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, -input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		set_animation("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	if raycaster.is_colliding():
-		var collider = raycaster.get_collider()
-
-		if collider.name == "Water":
-			if Input.is_action_just_pressed("interact"):
-				if Global.interact: # If fishing
-					if fish_hooked: 
-						catch_fish()
-					else: # Stop fishing
-						Global.interact = false
-						fishing = false
-						$Neck/Camera3D/Rod.visible = false
-				else: # Start fishing
-					if fishSpin.visible != true:
-						Global.interact = true
-						fishing = true
-						$Neck/Camera3D/Rod.visible = true
-
-
-	if fishing:
-		if !fish_hooked:
-			fishing_process()
+		set_animation("Idle")
 
 	if !Global.interact:
 		move_and_slide()
 
-func fishing_process():
-	if rng.randi_range(0,120 - Global.catch_speed_increase * 5) == 0:
-		fish_hooked = true
-		fishAlert.visible = true
 
-func catch_fish():
-	Global.interact = false
-	fishing = false
-	$Neck/Camera3D/Rod.visible = false
-	fishAlert.visible = false
-	fishSpin.visible = true
-	fishMusic.play(0)
+func set_animation(animation_name: String):
+	sprite.play(animation_name + sprite_direction)
+
+func update_sprite_direction():
+	var rot = wrapf(neck.rotation.y, 0, TAU)
+	
+	if rot >= 13.0*PI/8.0 or rot < 3.0*PI/8.0:
+		sprite_direction = "W"
+	elif rot >= 3.0*PI/8.0 and rot < 5.0*PI/8.0:
+		sprite_direction = "D"
+	elif rot >= 5.0*PI/8.0 and rot < 11.0*PI/8.0:
+		sprite_direction = "S"
+	else:
+		sprite_direction = "A"
